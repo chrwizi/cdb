8f56ec5747f5ecce3396858af6dfeb11af8718fd
chrwizi <superferi93@gmail.com>
2019-02-19 22:04:18 +0100

    modif local commit

--- projetCdb/src/main/java/app/projetCdb/dao/DbAccess.java
+++ projetCdb/src/main/java/app/projetCdb/dao/DbAccess.java
@@ -4,27 +4,27 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DbAccess implements IDbAccess{
-	private static DbAccess instance=new DbAccess();
-	private final static String DRIVER="com.mysql.cj.jdbc.Driver";
-	private final static String URL="jdbc:mysql://localhost:3306/computer-database-db?zeroDateTimeBehavior=convertToNull";
-	private final static String LOGIN="root";
-	private final static String PASSWORD="";
+public class DbAccess implements IDbAccess {
+	private static DbAccess instance = new DbAccess();
+	private final static String DRIVER = "com.mysql.cj.jdbc.Driver";
+	private final static String URL = "jdbc:mysql://localhost:3306/computer-database-db?zeroDateTimeBehavior=convertToNull";
+	private final static String LOGIN = "root";
+	private final static String PASSWORD = "";
 
 	private DbAccess() {
 		super();
 	}
 
 	public static DbAccess getInstance() {
-		return (instance==null)?new DbAccess():instance;
+		return (instance == null) ? new DbAccess() : instance;
 	}
 
 	public static void loadDriver() throws ClassNotFoundException {
 		Class.forName(DRIVER);
 	}
-	
+
 	@Override
 	public Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(URL,LOGIN,PASSWORD);
+		return DriverManager.getConnection(URL, LOGIN, PASSWORD);
 	}
 }
--- projetCdb/src/main/java/app/projetCdb/dao/IDbAccess.java
+++ projetCdb/src/main/java/app/projetCdb/dao/IDbAccess.java
@@ -4,5 +4,5 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public interface IDbAccess {
-	public Connection getConnection() throws SQLException ;
+	public Connection getConnection() throws SQLException;
 }
