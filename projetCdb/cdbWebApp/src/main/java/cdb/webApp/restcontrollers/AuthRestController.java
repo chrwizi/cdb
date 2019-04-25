580d2fdab8bb1b6e2518645f371b4773c07241c0
chrwizi <superferi93@gmail.com>
2019-04-25 14:08:27 +0200

    [add] create user restController

--- projetCdb/cdbWebApp/src/main/java/cdb/webApp/restcontrollers/AuthRestController.java
+++ projetCdb/cdbWebApp/src/main/java/cdb/webApp/restcontrollers/AuthRestController.java
@@ -7,46 +7,47 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
-import cdb.core.models.Role;
-import cdb.core.types.EnumRole;
 import cdb.core.types.TokenWraper;
 import cdb.security.jwt.TokenProvider;
 import cdb.service.UserService;
 import cdb.webApp.validator.LoginForm;
-import cdb.webApp.validator.SignUpForm;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("api/Auth")
+@CrossOrigin
+public class AuthRestController {
 
-public class User {
-	
 	private UserService userService;
 	private AuthenticationManager authenticationManager;
 	private TokenProvider tokenProvider;
-	
-	public User(UserService userService,TokenProvider tokenProvider, AuthenticationManager authenticationManager) {
+
+	public AuthRestController(UserService userService, TokenProvider tokenProvider,
+			AuthenticationManager authenticationManager) {
 		this.userService = userService;
 		this.authenticationManager = authenticationManager;
-		this.tokenProvider=tokenProvider;
+		this.tokenProvider = tokenProvider;
 	}
-	
 
-	@PostMapping("/auth")
+	@PostMapping("/login")
 	@CrossOrigin(origins = "*")
-	public void signIn(@Valid @RequestBody SignUpForm signUpForm) {	
-		userService.createUser(signUpForm.getUsername(), signUpForm.getPassword(), new Role(EnumRole.Premium));
+	public TokenWraper logIn(@Valid @RequestBody LoginForm loginForm) {
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginForm.getUsername(), loginForm.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwToken = tokenProvider.generateToken(authentication);
+		return new TokenWraper(jwToken);
 	}
-	
-	
+
+	@PostMapping("/logout")
+	@CrossOrigin(origins = "*")
+	public void LogOut(@RequestBody TokenWraper tokenWraper) {
+		
+	}
+
 }
-
-
-
